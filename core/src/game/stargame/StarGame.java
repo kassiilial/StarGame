2218ef32e80bd781cfe25ada63f9526835cfec58
ilya <kassirovilia@icloud.com>
2021-02-06 15:20:15 +0300

    Выполнение домашнего задания №2
    Картинка движется согласно заданию.

--- core/src/game/stargame/StarGame.java
+++ core/src/game/stargame/StarGame.java
@@ -1,33 +1,16 @@
 package game.stargame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+import game.stargame.screen.MenuScreen;
+
+public class StarGame extends Game {
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("fon.jpg");
+		setScreen(new MenuScreen());
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0.02184f, 0.10943f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 100, 100);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
