9bee16abf61daa0ad38e8b0a273d5552a884257b
ilya <kassirovilia@icloud.com>
2021-02-10 10:45:20 +0300

    Homework3. Вся логика размещена в файле Logo.

--- core/src/game/stargame/screen/MenuScreen.java
+++ core/src/game/stargame/screen/MenuScreen.java
@@ -7,17 +7,24 @@ import com.badlogic.gdx.math.Vector2;
 import game.stargame.base.BaseScreen;
 import game.stargame.math.Rect;
 import game.stargame.sprite.Background;
+import game.stargame.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
 
+    private Texture cat;
+    private Logo logo;
+
     @Override
     public void show() {
         super.show();
         bg = new Texture("texture/bg.png");
         background = new Background(bg);
+
+        cat = new Texture("fon.jpg");
+        logo = new Logo(cat);
     }
 
     @Override
@@ -26,11 +33,13 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
+        cat.dispose();
         bg.dispose();
         super.dispose();
     }
@@ -38,10 +47,12 @@ public class MenuScreen extends BaseScreen {
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        logo.touchDown(touch, pointer, button);
         return super.touchDown(touch, pointer, button);
     }
 }
